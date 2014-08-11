8da485f3173645977e78f3815402b62ae11e1973
Stefan Insam <ramsesoriginal@gmail.com>
2014-08-11 23:18:49 +0200

    Improved level, added GUI

---
+++ Assets/Music/JukeBox.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Music {
+	public class JukeBox : MonoBehaviour {
+
+		public AudioClip[] selection;
+
+		private AudioSource source;
+		private int currentsong;
+
+		void Start() {
+			source = (AudioSource) GetComponent<AudioSource>();
+			currentsong = 0;
+			source.clip = selection[currentsong];
+		}
+
+		// Update is called once per frame
+		void Update () {
+			if (!source.isPlaying) {
+				skip ();
+			}
+		}
+
+		void skip() {
+			source.Stop ();
+			currentsong = (currentsong + 1) & selection.Length;
+			source.clip = selection[currentsong];
+			source.Play();
+		}
+	}
+}
