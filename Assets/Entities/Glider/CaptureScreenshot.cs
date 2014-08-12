76ad8c1c7b9609a5fa8433cc88f9fb64e3a9c19d
Stefan Insam <ramsesoriginal@gmail.com>
2014-08-12 02:41:55 +0200

    Added the ability to take screenshots.
    Yaay, I guess...

---
+++ Assets/Entities/Glider/CaptureScreenshot.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaptureScreenshot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.J)) {
+			Application.CaptureScreenshot("Screenshot"+System.DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")+".png");
+		}
+		if (Input.GetKeyDown (KeyCode.K)) {
+			Application.CaptureScreenshot("Screenshot"+System.DateTime.Now.ToString("yyyy-dd-M--HH-mm-ss")+".png",3);
+		}
+	}
+}
